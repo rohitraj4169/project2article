ca8df49282c0368c4f19c6a7693e0d59f0cbedd4
rohitraj4169 <rohitraj@startbitsolutions.com>
2023-03-02 16:40:47 +0530

    Restrict Action

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,7 @@
 class ArticlesController < ApplicationController
 	before_action :find_params_by_id, only:[:show,:edit,:update,:destroy]
+	before_action :require_user, except: [:show, :index]
+  	before_action :require_same_user, only: [:edit, :update, :destroy]
 	def index
 		@article=Article.all
 	end
@@ -11,7 +13,7 @@ class ArticlesController < ApplicationController
 	end
 	def create
 		@article=Article.new(params_article_require)
-		@article.user=User.first
+		@article.user= current_user
 		if @article.save
 			flash[:notice]="Article  Created Successfully"
 			redirect_to @article
@@ -45,6 +47,13 @@ class ArticlesController < ApplicationController
 	 def params_article_require
 	 	params.require(:article).permit(:title,:description)
 	 end
+
+	def require_same_user
+		if current_user != @article.user
+			flash[:alert] = "You can only edit or delete your own article"
+			redirect_to @article
+		end
+  	end
 
 
 
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,19 +1,28 @@
 class UsersController < ApplicationController
+    before_action :set_user, only: [:show, :edit, :update, :destroy]
+    before_action :require_user, only: [:edit, :update]
+    before_action :require_same_user, only: [:edit, :update, :destroy]
     def new
         @user = User.new
     end
-    def edit
-        @user = User.find(params[:id])
+    def edit    
     end
+   
     def update
-        @user = User.find(params[:id])
         if @user.update(user_params)
             flash[:notice] = 'Account updated successfully '
-            redirect_to articles_path
+            redirect_to @user
         else
             render 'edit'
         end
-    
+    end
+
+    def index
+        @user = User.all
+    end
+
+    def show
+        @article=@user.articles
     end
 
     def create
@@ -25,6 +34,12 @@ class UsersController < ApplicationController
             render 'new'
         end
     end
+    def destroy
+        @user.destroy
+        session[:user_id] = nil
+        flash[:notice] = "Account and all associated articles successfully deleted"
+        redirect_to articles_path
+    end
 
     
 
@@ -32,5 +47,13 @@ class UsersController < ApplicationController
     def user_params
         params.require(:user).permit(:username, :email, :password)
     end 
-
+    def set_user
+        @user = User.find(params[:id])
+    end
+    def require_same_user
+    if current_user != @user
+      flash[:alert] = "You can only edit or delete your own Account"
+      redirect_to @user
+    end
+  end
 end
